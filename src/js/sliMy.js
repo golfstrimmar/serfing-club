a605dd1260884970d5dba79f83ca461f55fc0ebc
victor <ushinbox@gmail.com>
2020-05-25 03:24:28 +0300

    25.05 3.23

--- src/js/sliMy.js
+++ src/js/sliMy.js
@@ -36,3 +36,75 @@ $(Document).ready(function () {
     ],
   });
 }); 
+
+
+
+
+
+
+    $(Document).ready(function () {
+      $(".sliderbords-js").slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 3,     
+        // infinite: false,
+        slideToscroll: 1,
+        speed: 800, 
+        easing: "ease",
+        adapitveHeight: true,  
+        rows: 1,
+        // nextArrow: $(".control-slider-lots__arrow_next"),
+        // prevArrow: $(".control-slider-lots__arrow_prev"),
+        responsive: [
+          {
+            breakpoint: 819,
+            settings: {
+              // arrows: false,
+
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 620,
+            settings: {
+              // arrows: false,
+              // centerPadding: "40px",
+              slidesToShow: 1,
+            },
+          },
+        ],
+      });
+    });  
+    // $(Document).ready(function () {
+    //   $(".slider1-js").slick({
+    //     arrows: true,
+    //     dots: false,
+    //     slidesToShow: 1,     
+    //     // infinite: false,
+    //     slideToscroll: 1,
+    //     speed: 800, 
+    //     easing: "ease",
+    //     adapitveHeight: true,  
+    //     rows: 1,
+    //     // nextArrow: $(".control-slider-lots__arrow_next"),
+    //     // prevArrow: $(".control-slider-lots__arrow_prev"),
+    //     responsive: [
+    //       {
+    //         breakpoint: 819,
+    //         settings: {
+    //           // arrows: false,
+
+    //           slidesToShow: 2,
+    //         },
+    //       },
+    //       {
+    //         breakpoint: 620,
+    //         settings: {
+    //           // arrows: false,
+    //           // centerPadding: "40px",
+    //           slidesToShow: 1,
+    //         },
+    //       },
+    //     ],
+    //   });
+    // });  
